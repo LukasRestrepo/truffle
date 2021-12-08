ce5b3ba16a9e1766d9de9254fb3ec6673db40bc9
Lukas Restrepo <restrepolukas@gmail.com>
2021-12-08 01:56:01 -0500

    Updates

---
+++ precache-manifest.733b49c7df1009b32359573cd441212f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bb59a1857121086a33aa492ca1c1d9",
+    "url": "/truffle/index.html"
+  },
+  {
+    "revision": "ebad1c5c606567017257",
+    "url": "/truffle/static/css/main.54a0cdb6.chunk.css"
+  },
+  {
+    "revision": "f0925f39b0d76184c741",
+    "url": "/truffle/static/js/2.c93b412b.chunk.js"
+  },
+  {
+    "revision": "ebad1c5c606567017257",
+    "url": "/truffle/static/js/main.e01c49b0.chunk.js"
+  },
+  {
+    "revision": "71bdd16da574b8a376fb",
+    "url": "/truffle/static/js/runtime-main.73796a5d.js"
+  }
+]);
