e72af59853508d4ea04d15d65a803256658c2d84
Lukas Restrepo <restrepolukas@gmail.com>
2021-12-08 01:48:58 -0500

    changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const privateKey = "a9718dbcf53941049cd23a2de2eef408";
+const endpointUrl = "https://kovan.infura.io/v3/cadbb59d66f24264a74b8861c1154f57";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -9,6 +13,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "5777"
+    },
+    kovan: {
+      provider: () => new HDWalletProvider([privateKey], endpointUrl),
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
     }
   }
 };
