7b00ee91a01625890be8d6778dbb67ecd11c6aa8
Lukas Restrepo <restrepolukas@gmail.com>
2021-12-10 23:41:42 -0500

    kovan

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,17 +1,18 @@
 import Web3 from "web3";
-import ComplexStorage from "./contracts/ComplexStorage.json";
 import SimpleStorage from "./contracts/SimpleStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
 
 const options = {
   web3: {
     block: false,
-    customProvider: new Web3("ws://localhost:8545"),
+    //customProvider: new Web3("ws://localhost:8545"),
+    customProvider: new Web3(Web3.givenProvider),
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+  // contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+  contracts: [SimpleStorage],
+  syncAlways: false,
   events: {
     SimpleStorage: ["StorageSet"],
-  },
+  }
 };
 
 export default options;
--- truffle-config.js
+++ truffle-config.js
@@ -1,24 +1,24 @@
 const path = require("path");
 
+const fs = require('fs');
+const secrets = JSON.parse(fs.readFileSync(path.join(__dirname, '.secrets.json')).toString().trim());
+
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const privateKey = "a9718dbcf53941049cd23a2de2eef408";
-const endpointUrl = "https://kovan.infura.io/v3/cadbb59d66f24264a74b8861c1154f57";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
-    develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "5777"
-    },
     kovan: {
-      provider: () => new HDWalletProvider([privateKey], endpointUrl),
-      gas: 5000000,
-      gasPrice: 25000000000,
+      provider: () => new HDWalletProvider({
+        mnemonic: { phrase: secrets.mnemonic },
+        providerOrUrl: `https://kovan.infura.io/v3/${secrets.projectId}`
+      }),
+      gas: 4712388,
+      gasPrice: 100000000000,
       network_id: 42
     }
   }
-};
+}
